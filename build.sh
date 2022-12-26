0c4adc659582ce3cd3728d620dcc72e735a9e3b1
jaonoctus <jaonoctus@protonmail.com>
2022-12-26 02:53:57 -0300

    feat(build.sh): create checksums and signature

--- build.sh
+++ build.sh
@@ -2,15 +2,27 @@
 
 build() {
     local output="hh"
+    local output_folder="./bin"
     local targets="node18-linux-x64,node18-linux-arm64"
 
-    pkg -C GZip \
-        -o "bin/${output}" \
+    printf "# START: building binaries...\n"
+    [ -d bin ] && rm -rf ./bin
+    npx pkg -C GZip \
+        -o "${output_folder}/${output}" \
         -t "${targets}" \
         --no-bytecode \
         --public-packages "*" \
         --public \
         src/index.js
+    printf "# DONE: binaries built.\n"
+    
+    printf "# START: generating checksums...\n"
+    sha256sum "${output_folder}/"* > ./bin/SHA256SUMS
+    printf "# DONE: SHA256SUMS generated.\n"
+
+    printf "# START: creating a signature...\n"
+    gpg --detach-sign --armor ./bin/SHA256SUMS
+    printf "# DONE: checksum file signed.\n"
 }
 
 build
